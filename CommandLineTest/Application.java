697b2161fe2ce4834feb041e0c2e9d4a49a51104
MichaelKirl <michael.e.kirl.mil@swf.army.mil>
2022-02-24 09:42:42 -0600

    configured gradle

--- CommandLineTest/Application.java
+++ CommandLineTest/Application.java
@@ -1,8 +1,5 @@
-package com.galvanize;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Test 2");
     }
 }
